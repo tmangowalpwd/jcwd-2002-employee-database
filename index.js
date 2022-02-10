f0efbbcfd2c8b3fedcd6036f1a6515f1de9e7b07
tmangowal <tmangowal@gmail.com>
2022-02-10 14:30:57 +0700

    updated challenges

---
+++ index.js
@@ -0,0 +1,13 @@
+// Duplikasi method .find
+// Duplikasi method .indexOf
+
+// Conditions (indexOf)
+// 1. Mencari index dari sebuah item dalam array
+// 2. Akan return sebuah number (index)
+
+// Conditions (find)
+// 1. Mencari sebuah value (item) dalam array yang
+//    memenuhi condition yang kita berikan
+// 2. Callback function kita harus return boolean
+// 3. Ketika callback function return true, loop akan berhenti
+//    lalu function find akan return item dari array.
